c25bf0fc6249483a2da6e709fb9da7ab6af4e9ad
subhalakshmi-22 <139205282+subhalakshmi-22@users.noreply.github.com>
2023-07-13 09:36:25 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+var on_btn=document.getElementById("on");
+var off_btn=document.getElementById("off");
+var img=document.getElementById("bulb");
+
+function bulb_on(){
+    img.src="./images/bulb_on.jpeg";
+
+}
+
+function bulb_off(){
+    img.src="./images/bulb_off.jpeg";
+
+}
